355d35bdced2b4f83a0a28640af88ae5aa321efb
slimzim <slimzim@gmail.com>
2020-07-09 12:12:44 -0500

    finished all logic, cleaned up formatting, added user name to modal header

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -30,27 +30,25 @@ function compareFriends(userData) {
             
              // after the outer for loop finishes comparing the scores array...
              if (j+1 === friendsArray[i].scores.length) {
-                 // we set the first friend to the best match by default 
+                // the first friend to the best match by default 
                 if (i === 0) {
                     bestMatchValue = totalDifference
                     // we keep track of the best match's index in the friendsArray
                     bestMatchIndex = i                   
-                } 
+                }
                 // if any subsequent friends are more similar...
                 else if (totalDifference < bestMatchValue) {
-                    // they becomes the best match, and their index is stored
+                    // that friend becomes the best match, and their index is stored
                     bestMatchValue = totalDifference
                     bestMatchIndex = i    
                 } 
                 // totalDifference is reset for the next friend
                 totalDifference = 0
-                }
-                
-            }
-            
+                } 
+            }            
         }
-        // when the original for loop finishes, the function returns all of the friend's data by 
-        // referring to that friend's index in the friendsArray
+        // when the original for loop finishes, the function returns the best matched 
+        // friend's data by referring to that friend's index in the friendsArray
         return friendsArray[bestMatchIndex]
     }
 
